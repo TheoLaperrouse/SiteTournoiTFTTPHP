114b71ba68c5551d2b0d411895287292f6eaf267
Théo Laperrouse <theo.laperrouse@laposte.net>
2021-11-19 00:14:03 +0100

    fix SQL script

---
+++ CreateTableTableaux.sql
@@ -0,0 +1,27 @@
+CREATE TABLE `tableaux` (
+`name` VARCHAR(50),
+`place` VARCHAR(50),
+`huitieme` VARCHAR(50),
+`quart` VARCHAR(50),
+`demi` VARCHAR(50),
+`finaliste` VARCHAR(50),
+`vainqueur` VARCHAR(50),
+`dateDebut` VARCHAR(50),
+`pointMax` VARCHAR(50),
+PRIMARY KEY (`numLicence`)
+);
+
+INSERT INTO tableaux VALUES ('tableauA', '72','','','','','','','1400');
+INSERT INTO tableaux VALUES ('tableauB', '72','','','','','','','900');
+INSERT INTO tableaux VALUES ('tableauC', '72','','','','','','','1900');
+INSERT INTO tableaux VALUES ('tableauD', '72','','','','','','','1200');
+INSERT INTO tableaux VALUES ('tableauE', '72','','','','','','','1600');
+INSERT INTO tableaux VALUES ('tableauF', '72','','','','','','','2100');
+INSERT INTO tableaux VALUES ('tableauG', '36','','','','','','','3500');
+INSERT INTO tableaux VALUES ('tableauH', '72','','','','','','','1500');
+INSERT INTO tableaux VALUES ('tableauI', '72','','','','','','','1000');
+INSERT INTO tableaux VALUES ('tableauJ', '72','','','','','','','2500');
+INSERT INTO tableaux VALUES ('tableauK', '72','','','','','','','1300');
+INSERT INTO tableaux VALUES ('tableauL', '72','','','','','','','1800');
+INSERT INTO tableaux VALUES ('tableauM', '72','','','','','','','3500');
+INSERT INTO tableaux VALUES ('tableauN', '36','','','','','','','3500');
